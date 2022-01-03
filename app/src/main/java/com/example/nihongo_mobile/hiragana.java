306784ebc72e3ddd2582dbb4f261ad302812512c
FarizAhmadBZ <fariz.berlian@gmail.com>
2022-01-04 06:56:31 +0700

    Update deklarasi button
    Button halaman hiragana + katakana sudah dideclare semua, sisa assign

--- app/src/main/java/com/example/nihongo_mobile/hiragana.java
+++ app/src/main/java/com/example/nihongo_mobile/hiragana.java
@@ -3,14 +3,27 @@ package com.example.nihongo_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class hiragana extends AppCompatActivity {
-
+    Button btnA, btnI, btnU, btnE, btnO,
+            btnKA, btnKI, btnKU, btnKE, btnKO,
+            btnSA, btnSI, btnSU, btnSE, btnSO,
+            btnTA, btnTI, btnTU, btnTE, btnTO,
+            btnNA, btnNI, btnNU, btnNE, btnNO,
+            btnHA, btnHI, btnHU, btnHE, btnHO,
+            btnMA, btnMI, btnMU, btnME, btnMO,
+            btnYA, btnYU, btnYO,
+            btnRA, btnRI, btnRU, btnRE, btnRO,
+            bntWA, btnWO, btnN,
+            btnGA, btnGI, btnGU, btnGE, btnGO,
+            btnZA, btnZI, btnZU, btnZE, btnZO,
+            btnDA, btnDI, btnDU, btnDE, btnDO,
+            btnBA, btnBI, btnBU, btnBE, btnBO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hiragana);
-
         getSupportActionBar().hide();
     }
 }
