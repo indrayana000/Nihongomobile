2d69c07064e46fc65d230602177f6c406d348c19
FarizAhmadBZ <fariz.berlian@gmail.com>
2022-01-04 08:23:46 +0700

    Update
    + Katakana Voice A-O done
    + Asset suara huruf vokal, K, dan H sudah dimasukkan

--- app/src/main/java/com/example/nihongo_mobile/halaman_audio.java
+++ app/src/main/java/com/example/nihongo_mobile/halaman_audio.java
@@ -2,14 +2,76 @@ package com.example.nihongo_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class halaman_audio extends AppCompatActivity {
-
+    Button btnAudio;
+    String huruf, baca, idSuara;
+    TextView tvHuruf, tvBaca;
+    int voiceId;
+    MediaPlayer mp;
+    ArrayList<Integer> voices;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_audio);
         getSupportActionBar().hide();
+        btnAudio = findViewById(R.id.button_play);
+        tvHuruf = findViewById(R.id.textViewHurufJepang);
+        tvBaca = findViewById(R.id.textViewRomaji);
+        idSuara = getIntent().getExtras().getString("voiceId").toString();
+        voiceId = Integer.valueOf(idSuara);
+        voices = new ArrayList<>();
+        declareAudio();
+        huruf = getIntent().getExtras().getString("huruf").toString();
+        baca = getIntent().getExtras().getString("baca").toString();
+
+        tvHuruf.setText(huruf);
+        tvBaca.setText(baca);
+
+
+        mp = MediaPlayer.create(getApplicationContext(), voices.get(voiceId));
+        btnAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    mp.seekTo(0);
+                    mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mediaPlayer) {
+                            mp.seekTo(0);
+                        }
+                    });
+                    mp.start();
+                }catch (Exception e) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "error: " + e, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        });
+    }
+    protected void declareAudio(){
+        try {
+            voices.add(0, R.raw.a);
+            voices.add(1, R.raw.i);
+            voices.add(2, R.raw.u);
+            voices.add(3, R.raw.e);
+            voices.add(4, R.raw.o);
+            voices.add(5, R.raw.ka);
+            voices.add(6, R.raw.ki);
+            voices.add(7, R.raw.ku);
+            voices.add(8, R.raw.ke);
+            voices.add(9, R.raw.ko);
+        }catch(Exception e){
+            Toast.makeText(getApplicationContext(), "Error + " + e, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/nihongo_mobile/katakana.java
+++ app/src/main/java/com/example/nihongo_mobile/katakana.java
@@ -2,8 +2,11 @@ package com.example.nihongo_mobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class katakana extends AppCompatActivity {
     Button btnA, btnI, btnU, btnE, btnO,
@@ -20,10 +23,114 @@ public class katakana extends AppCompatActivity {
             btnZA, btnZI, btnZU, btnZE, btnZO,
             btnDA, btnDI, btnDU, btnDE, btnDO,
             btnBA, btnBI, btnBU, btnBE, btnBO;
+    String setHuruf, setBaca, setVoiceID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_katakana);
         getSupportActionBar().hide();
+        declareButton();
+
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setHuruf = "ア";
+                setBaca = "A";
+                setVoiceID = "0";
+                pindahHalaman(setHuruf , setBaca, setVoiceID);
+            }
+        });
+
+        btnI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setHuruf = "イ";
+                setBaca = "I";
+                setVoiceID = "1";
+                pindahHalaman(setHuruf , setBaca, setVoiceID);
+            }
+        });
+        btnU.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setHuruf = "ウ";
+                setBaca = "U";
+                setVoiceID = "2";
+                pindahHalaman(setHuruf , setBaca, setVoiceID);
+            }
+        });
+        btnE.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setHuruf = "エ";
+                setBaca = "E";
+                setVoiceID = "3";
+                pindahHalaman(setHuruf , setBaca, setVoiceID);
+            }
+        });
+        btnO.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setHuruf = "オ";
+                setBaca = "O";
+                setVoiceID = "4";
+                pindahHalaman(setHuruf , setBaca, setVoiceID);
+            }
+        });
+
+    }
+    protected void pindahHalaman(String huruf, String baca, String voiceId){
+        try {
+            Intent pindahBaca = new Intent(katakana.this, halaman_audio.class);
+            pindahBaca.putExtra("huruf", huruf);
+            pindahBaca.putExtra("baca", baca);
+            pindahBaca.putExtra("voiceId", voiceId);
+            startActivity(pindahBaca);
+        }catch(Exception e){
+
+        }
+
+    }
+    protected void declareButton(){
+        btnA = findViewById(R.id.buttonkatakana_a);
+        btnI = findViewById(R.id.buttonkatakana_i);
+        btnU = findViewById(R.id.buttonkatakana_u);
+        btnE = findViewById(R.id.buttonkatakana_e);
+        btnO = findViewById(R.id.buttonkatakana_o);
+        btnKA = findViewById(R.id.buttonkatakana_ka);
+        btnKI = findViewById(R.id.buttonkatakana_ki);
+        btnKU = findViewById(R.id.buttonkatakana_ku);
+        btnKE = findViewById(R.id.buttonkatakana_ke);
+        btnKO = findViewById(R.id.buttonkatakana_ko);
+        btnSA = findViewById(R.id.buttonkatakana_sa);
+        btnSI = findViewById(R.id.buttonkatakana_si);
+        btnSU = findViewById(R.id.buttonkatakana_su);
+        btnSE = findViewById(R.id.buttonkatakana_se);
+        btnSO = findViewById(R.id.buttonkatakana_so);
+        btnTA = findViewById(R.id.buttonkatakana_ta);
+        btnTI = findViewById(R.id.buttonkatakana_ti);
+        btnTU = findViewById(R.id.buttonkatakana_tu);
+        btnTE = findViewById(R.id.buttonkatakana_te);
+        btnTO = findViewById(R.id.buttonkatakana_to);
+        btnNA = findViewById(R.id.buttonkatakana_na);
+        btnNI = findViewById(R.id.buttonkatakana_ni);
+        btnNU = findViewById(R.id.buttonkatakana_nu);
+        btnNE = findViewById(R.id.buttonkatakana_ne);
+        btnNO = findViewById(R.id.buttonkatakana_no);
+        btnHA = findViewById(R.id.buttonkatakana_ha);
+        btnHI = findViewById(R.id.buttonkatakana_hi);
+        btnHU = findViewById(R.id.buttonkatakana_hu);
+        btnHE = findViewById(R.id.buttonkatakana_he);
+        btnHO = findViewById(R.id.buttonkatakana_ho);
+        btnMA = findViewById(R.id.buttonkatakana_ma);
+        btnMI = findViewById(R.id.buttonkatakana_mi);
+        btnMU = findViewById(R.id.buttonkatakana_mu);
+        btnME = findViewById(R.id.buttonkatakana_me);
+        btnMO = findViewById(R.id.buttonkatakana_mo);
+        btnRA = findViewById(R.id.buttonkatakana_ra);
+        btnRI = findViewById(R.id.buttonkatakana_ri);
+        btnRU = findViewById(R.id.buttonkatakana_ru);
+        btnRE = findViewById(R.id.buttonkatakana_re);
+        btnRO = findViewById(R.id.buttonkatakana_ro);
     }
 }
